8cea4a5fb7ca9fb65bed2e1b8d4bfa704e15b8fd
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-11-29 21:17:33 +0800

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.huricanez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Vector;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    int[] mas1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        et = findViewById(R.id.et);
+    }
+
+    public void onClick(View view){
+        String temp = et.toString();
+        String[] mas = temp.split(" ");
+        mas1 = new int[mas.length];
+        for(int i = 0 ; i<mas1.length; i++){
+            mas1[i] = Integer.parseInt(mas[i]);
+        }
+    }
+
+}
