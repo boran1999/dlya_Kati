458e71aa30a46cc753c7615e3e8e1c45230dc417
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-11-29 21:24:07 +0800

    Update Diagram.java

--- Diagram.java
+++ Diagram.java
@@ -12,19 +12,19 @@ import androidx.annotation.Nullable;
 
 public class Diagram extends View {
     Path sector = new Path();
-    MainActivity activity;
+    MainActivity activity;   // делам так чтоб можно было передавать объекты из MainAcctivity
     public Diagram(Context context) {
         super(context);
     }
 
-    public Diagram(Context context, @Nullable AttributeSet attrs) {
+    public Diagram(Context context, @Nullable AttributeSet attrs) { // пытаемся просуммировать все числа из тех что ввел пльзователь
         super(context, attrs);
-        activity = (MainActivity) context;
+        activity = (MainActivity) context;   // оращаемся к МейнАктивити
         int sum= 0;
         for(int i =0; i< activity.mas1.length;i++){
             sum = sum + activity.mas1[i];
         }
-        for(int i =0; i< activity.mas1.length;i++){
+        for(int i =0; i< activity.mas1.length;i++){ // переприсваеваем значения так, чтоб теперь они стали процентами от диаграммы
             activity.mas1[i]=activity.mas1[i]/sum *360;
         }
     }
@@ -40,7 +40,7 @@ public class Diagram extends View {
 
         // отображаем контур на холсте
         canvas.drawPath(sector, p);
-        for(int i=0; i<activity.mas1.length;i++){
+        for(int i=0; i<activity.mas1.length;i++){ // пытаемся отобраить, но это не работает
             canvas.drawArc(0, 0, w, w ,0 ,activity.mas1[i], true, p);
         }
 
